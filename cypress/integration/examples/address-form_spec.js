f3fcf6cf5d8d4735c0a8cdd2a07e292b68ed597f
hajaniain <hajaniainamarius@gmail.com>
2021-01-20 18:13:08 +0100

    update spec test

--- cypress/integration/examples/address-form_spec.js
+++ cypress/integration/examples/address-form_spec.js
@@ -1,19 +1,19 @@
 describe("My First Test", () => {
-  it("Visits the Kitchen Sink", () => {
+  it("Fill address form", () => {
     cy.visit("http://localhost:4200");
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#mat-input-0').click();
-    cy.get('#mat-input-0').type('test');
-    cy.get('#mat-input-1').type('john');
-    cy.get('#mat-input-2').type('doe');
-    cy.get('.col > .mat-focus-indicator > .mat-button-wrapper').click();
-    cy.get('#mat-input-6').click();
-    cy.get('#mat-input-4').click();
-    cy.get('#mat-input-4').type('paris');
-    cy.get('.mat-select-placeholder').click();
-    cy.get('#mat-option-1 > .mat-option-text').click();
-    cy.get('#mat-input-5').click();
-    cy.get('#mat-input-5').type('75');
+    cy.get("#mat-input-0").click();
+    cy.get("#mat-input-0").type("test");
+    cy.get("#mat-input-1").type("john");
+    cy.get("#mat-input-2").type("doe");
+    cy.get(".col > .mat-focus-indicator > .mat-button-wrapper").click();
+    cy.get("#mat-input-6").click();
+    cy.get("#mat-input-4").click();
+    cy.get("#mat-input-4").type("paris");
+    cy.get(".mat-select-placeholder").click();
+    cy.get("#mat-option-1 > .mat-option-text").click();
+    cy.get("#mat-input-5").click();
+    cy.get("#mat-input-5").type("75");
     /* ==== End Cypress Studio ==== */
   });
 });
